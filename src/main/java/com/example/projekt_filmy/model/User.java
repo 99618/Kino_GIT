b8881f6a08bd27985a22011de76df70dc21d34d0
99618 <s99618@pollub.edu.pl>
2025-12-07 23:38:01 +0100

    feat(99618): models User, Movie, Review

---
+++ src/main/java/com/example/projekt_filmy/model/User.java
@@ -0,0 +1,43 @@
+package com.example.projekt_filmy.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 1, max=30)
+    private String firstName;
+
+    @NotBlank
+    @Size(min = 1, max=30)
+    private String lastName;
+
+    @Email
+    @NotBlank
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.USER;
+
+    public enum Role{
+        USER, ADMIN
+    }
+}
