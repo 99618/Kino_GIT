1d6225b5824d1ba2841fcec809b1b859a0e85095
99618 <s99618@pollub.edu.pl>
2025-12-07 23:31:05 +0100

    Inicjalizacja: git init + build, docker-compose, app

---
+++ src/main/java/com/example/projekt_filmy/ProjektFilmyApplication.java
@@ -0,0 +1,13 @@
+package com.example.projekt_filmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektFilmyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektFilmyApplication.class, args);
+	}
+
+}
